cb818c549175358f6904fde2d399e21e73b1eef6
Narciso <fabrizio.narciso@gmail.com>
2024-01-24 10:09:47 -0300

    added xcode project

---
+++ WWDC2024/WWDC2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC2024
+//
+//  Created by Fabrizio Narciso on 24/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC2024/WWDC2024/WWDC2024App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2024App.swift
+//  WWDC2024
+//
+//  Created by Fabrizio Narciso on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
